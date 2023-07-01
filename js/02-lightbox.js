a41c30efd4c63a318ab2d8795960781a9abf7755
vladbevz <bevz.vlad15@gmail.com>
2023-07-01 22:04:28 +0200

    update task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+//
+const galleryList = document.querySelector(".gallery");
+createImagesItems();
 
-console.log(galleryItems);
+function createImagesItems() {
+  const importImages = createImagesEl(galleryItems);
+  galleryList.innerHTML = importImages.join("");
+}
+function createImagesEl(items) {
+  return items.map(
+    (item) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `
+  );
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionData: "alt",
+  captionDelay: "250",
+});
